a70aa9cce526e217e3be6832dc4b4ed46f255431
mirobo <78788210+mirobo@users.noreply.github.com>
2024-09-24 19:02:23 +0000

    set parseOptions resolveFully + resolveCombinators to true -> it works! :)

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,9 +2,7 @@
 package org.example;
 
 import java.io.File;
-import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -14,120 +12,123 @@ import java.util.Map;
 import org.openapi4j.core.model.v3.OAI3;
 import org.openapi4j.core.model.v3.OAI3Context;
 import org.openapi4j.core.util.TreeUtil;
-import org.openapi4j.core.validation.ValidationResults;
 import org.openapi4j.operation.validator.model.impl.Body;
 import org.openapi4j.operation.validator.validation.OperationValidator;
 import org.openapi4j.parser.OpenApi3Parser;
 import org.openapi4j.parser.model.v3.OpenApi3;
-import org.openapi4j.parser.validation.v3.OpenApi3Validator;
 import org.openapi4j.schema.validator.ValidationContext;
 import org.openapi4j.schema.validator.ValidationData;
 import org.openapi4j.schema.validator.v3.ValidationOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.atlassian.oai.validator.OpenApiInteractionValidator;
 import com.atlassian.oai.validator.model.Request.Method;
 import com.atlassian.oai.validator.model.Response;
 import com.atlassian.oai.validator.model.SimpleResponse;
-import com.atlassian.oai.validator.report.LevelResolver;
 import com.atlassian.oai.validator.report.ValidationReport;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import io.swagger.v3.parser.core.models.ParseOptions;
+
 public class App {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        validateReponse("""
-                {
-                  "bark": true,
-                  "breed": "Dingo"
-                }
-                """, "./src/main/resources/oas.yaml", "/oneOf");
+    validateReponse("""
+        {
+          "bark": true,
+          "breed": "Dingo"
+        }
+        """, "./src/main/resources/oas.yaml", "/oneOf");
 
-        validateReponse("""
-                {
-                  "result": {
-                    "whatever": "so what"
-                  },
-                  "messages": {
-                    "extendedErrorModel": {
-                      "code": 100,
-                      "message": "hoi",
-                      "rootCause": "hey"
-                    }
-                  }
-                }
-                """, "./src/main/resources/oas.yaml", "/allOf");
+    validateReponse("""
+        {
+          "result": {
+            "whatever": "so what"
+          },
+          "messages": {
+            "extendedErrorModel": {
+              "code": 100,
+              "message": "hoi",
+              "rootCause": "hey"
+            }
+          }
+        }
+        """, "./src/main/resources/oas.yaml", "/allOf");
 
-    }
+  }
 
-    public static void validateReponse(String response, String oasSpecFile, String path) throws Exception {
-        OpenApiInteractionValidator val = OpenApiInteractionValidator.createForSpecificationUrl(oasSpecFile).build();
+  public static void validateReponse(String response, String oasSpecFile, String path) throws Exception {
 
-        // .withLevelResolver(
-        // LevelResolver.create()
-        // .withLevel("validation.request.body.schema.additionalProperties",
-        // ValidationReport.Level.IGNORE)
-        // .build())
+    final ParseOptions parseOptions = new ParseOptions();
+    parseOptions.setResolveFully(true);
+    parseOptions.setResolveCombinators(true);
 
-        // atlassian swagger request validator
-        Response res = SimpleResponse.Builder.ok().withBody(response).withContentType("application/json").build();
-        ValidationReport report = val.validateResponse(path, Method.GET, res);
+    OpenApiInteractionValidator val = OpenApiInteractionValidator.createForSpecificationUrl(oasSpecFile)
+        .withParseOptions(parseOptions).build();
 
-        System.out.println("Validate response: " + response);
-        System.out.println("\r\natlassian validator");
-        System.out.println("has errors: " + report.hasErrors());
-        report.getMessages().forEach(System.out::println);
+    // .withLevelResolver(
+    // LevelResolver.create()
+    // .withLevel("validation.request.body.schema.additionalProperties",
+    // ValidationReport.Level.IGNORE)
+    // .build())
 
-        // openapi4j validator
-        URL OAS_SPEC_FILE_AS_URL = new File(oasSpecFile).getAbsoluteFile().toURI().toURL();
-        OpenApi3 api = new OpenApi3Parser().parse(new File(oasSpecFile), true);
-        JsonNode schemaNode = TreeUtil.load(OAS_SPEC_FILE_AS_URL);
-        OAI3Context apiContext = new OAI3Context(OAS_SPEC_FILE_AS_URL, schemaNode);
-        ValidationContext<OAI3> validationContext = new ValidationContext<>(apiContext);
-        validationContext.setOption(ValidationOptions.ADDITIONAL_PROPS_RESTRICT, true);
-        OperationValidator val2 = new OperationValidator(validationContext, api, api.getPath(path),
-                api.getPath(path).getOperation("get"));
-        ValidationData<Void> validationData = new ValidationData<>();
-        val2.validateResponse(new Response2(response), validationData);
-        System.out.println("\r\nopenapi4j validator");
-        System.out.println("has errors: " + !validationData.results().isValid());
-        validationData.results().items().forEach(System.out::println);
-    }
+    // atlassian swagger request validator
+    Response res = SimpleResponse.Builder.ok().withBody(response).withContentType("application/json").build();
+    ValidationReport report = val.validateResponse(path, Method.GET, res);
+
+    System.out.println("Validate response: " + response);
+    System.out.println("\r\natlassian validator");
+    System.out.println("has errors: " + report.hasErrors());
+    report.getMessages().forEach(System.out::println);
+
+    // openapi4j validator
+    URL OAS_SPEC_FILE_AS_URL = new File(oasSpecFile).getAbsoluteFile().toURI().toURL();
+    OpenApi3 api = new OpenApi3Parser().parse(new File(oasSpecFile), true);
+    JsonNode schemaNode = TreeUtil.load(OAS_SPEC_FILE_AS_URL);
+    OAI3Context apiContext = new OAI3Context(OAS_SPEC_FILE_AS_URL, schemaNode);
+    ValidationContext<OAI3> validationContext = new ValidationContext<>(apiContext);
+    validationContext.setOption(ValidationOptions.ADDITIONAL_PROPS_RESTRICT, true);
+    OperationValidator val2 = new OperationValidator(validationContext, api, api.getPath(path),
+        api.getPath(path).getOperation("get"));
+    ValidationData<Void> validationData = new ValidationData<>();
+    val2.validateResponse(new Response2(response), validationData);
+    System.out.println("\r\nopenapi4j validator");
+    System.out.println("has errors: " + !validationData.results().isValid());
+    validationData.results().items().forEach(System.out::println);
+  }
 
 }
 
 class Response2 implements org.openapi4j.operation.validator.model.Response {
 
-    String response;
+  String response;
 
-    public Response2(String response) {
-        this.response = response;
-    }
+  public Response2(String response) {
+    this.response = response;
+  }
 
-    @Override
-    public int getStatus() {
-        return 200;
-    }
+  @Override
+  public int getStatus() {
+    return 200;
+  }
 
-    @Override
-    public Body getBody() {
-        return Body.from(this.response);
-    }
+  @Override
+  public Body getBody() {
+    return Body.from(this.response);
+  }
 
-    @Override
-    public Map<String, Collection<String>> getHeaders() {
-        return new HashMap<>();
-    }
+  @Override
+  public Map<String, Collection<String>> getHeaders() {
+    return new HashMap<>();
+  }
 
-    @Override
-    public Collection<String> getHeaderValues(String name) {
-        List<String> headers = new ArrayList<>();
-        if ("Content-Type".equals(name)) {
-            headers.add("application/json");
-        }
-        return headers;
+  @Override
+  public Collection<String> getHeaderValues(String name) {
+    List<String> headers = new ArrayList<>();
+    if ("Content-Type".equals(name)) {
+      headers.add("application/json");
     }
+    return headers;
+  }
 
 }
